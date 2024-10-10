b2c1c7df6338e44cc8e0bef2872d45244998e75f
Melahel1 <mjfloreschu2004@gmail.com>
2024-10-10 22:32:30 +0800

    Svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({ pages: "build", assets: "build", fallback: undefined, precompress: false, strict: true }),
+        prerender: {
+            handleHttpError: 'warn', // Options: 'warn', 'continue', 'ignore'
+        }
+    },
 };
 
 export default config;
