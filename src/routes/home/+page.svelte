c8c1ef8e56fc748cbbaa83cf995a7f9dd5db57a7
Melahel1 <mjfloreschu2004@gmail.com>
2024-10-11 00:05:47 +0800

    svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -129,7 +129,7 @@
             <ul>
                 <li><a href="/home">Home</a></li>
                 <li><a href="/about">About</a></li>
-                <li><a href="/project">Project</a></li>
+                <li><a href="/portfolio">Portfolio</a></li>
                 <li><a href="/contact">Contacts</a></li>
             </ul>
         </nav>
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -10,7 +10,7 @@
         <ul>
             <li><a href="/home">Home</a></li>
             <li><a href="/about">About</a></li>
-            <li><a href="/project">Project</a></li>
+            <li><a href="/portfolio">Portfolio</a></li>
             <li><a href="/contacts">Contact</a></li>
         </ul>
     </nav>
